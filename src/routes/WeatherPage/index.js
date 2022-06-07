84e28e5af424300cef4a7765eb54954922e903d0
Анастасия Кол <akol@axbit.ru>
2022-06-07 22:53:07 +0300

    получение json, преобразование объекта

--- src/routes/WeatherPage/index.js
+++ src/routes/WeatherPage/index.js
@@ -6,27 +6,49 @@ const WeatherPage = () => {
     
      // fetch("")
        
-     const [users, setUsers] = useState([])
+     const [weather, setWeather] = useState([])
 
      const fetchData = () => {
-       fetch("https://api.openweathermap.org/data/3.0/onecall?lat=51.67736807571518&lon=39.19305461001669&exclude=hourly&appid=53b2874620ba463264e4a8754d293e06")
-         .then(response => {
+      fetch("http://api.openweathermap.org/data/2.5/onecall?lat=51.67736807571518&lon=39.19305461001669&lang=ru&exclude=hourly&appid=53b2874620ba463264e4a8754d293e06")
+    
+      .then(response => {
            return response.json()
          })
          .then(data => {
-           setUsers(data)
+           setWeather(data)
+           
+           
+         
          })
+       
+        
      }
    
      useEffect(() => {
        fetchData()
      }, [])
 
+    const currentWeather = Object.values(weather);
+    const currentWeatherValues = currentWeather[4];
+
+
+      // const weather = Array.from(weatherObj);
     return (
         <>
               <Header title="weatherPage"></Header>
+              <div key={weather.current}>
+
+                 {/* { console.log(weather.current)} */}
+                  </div>
                 <p> 
-                    This is WeatherPage;
+          
+                    {/* {  console.log("2")}
+                   {  console.log(Object.values(weather))} */}
+                   {  console.log(currentWeatherValues)}  
+                   {  console.log("currentWeatherValues")}  
+                   {  console.log(currentWeather[4])}  
+                   {console.log("1")}
+           
                 </p>
         </>
         
